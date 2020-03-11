c7f4f4b9dda0a027e505545ee2a69feea27e3b9a
atlasf <emirhanvci@outlook.com>
2020-03-11 18:10:55 +0300

    Updates

---
+++ precache-manifest.8c95d61eff7f9f1199f8816a4fb3c00d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e052dd14a0ae61d28fba34009a21260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cca9211929fb1fae0a55",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "765a3af75dafc761efe9",
+    "url": "/static/js/2.5b14aa26.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5b14aa26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca9211929fb1fae0a55",
+    "url": "/static/js/main.c38e274d.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "986831ea4bf99fcc8ea266a2fa78357c",
+    "url": "/static/media/profileson.986831ea.jpg"
+  }
+]);
